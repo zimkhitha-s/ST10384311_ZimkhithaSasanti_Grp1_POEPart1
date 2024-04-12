ff9e0ddaf3df1cfa4f01b859002cf4e123171d7d
Zimkhitha Sasanti <130903306+ZimkhtihaOlgaSasanti@users.noreply.github.com>
2024-04-12 18:18:15 +0200

    Step 2: Created the public global variables and arrays that is required for the application

--- RecipeInformation.cs
+++ RecipeInformation.cs
@@ -8,5 +8,17 @@ namespace SanelesRecipeApplication
 {
     internal class RecipeInformation
     {
+        // The variables for the RecipeInformation Class
+        public string recipeDetails;
+        public int numberOfIngredients;
+        public string description;
+        // The ingredientsQuantity Array containing the quantity for the recipe ingredients
+        public int[] ingredientsQuantity;
+        // The ingredientsUnitOfMeasurement Array containing the unit of measurement for the recipe ingredients
+        public string[] ingredientsUnitOfMeasurement;
+        // The ingredientsName Array containing the names of the recipe ingredients
+        public string[] ingredientsName;
+        // The numberOfSteps Array containing the number of steps for the recipe
+        public int[] numberOfSteps;
     }
 }
