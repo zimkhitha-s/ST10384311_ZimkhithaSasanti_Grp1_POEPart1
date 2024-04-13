75556d2f452107af3c23e6586c51d0c30460f010
Zimkhitha Sasanti <130903306+ZimkhtihaOlgaSasanti@users.noreply.github.com>
2024-04-13 11:28:17 +0200

    Step 5: Created a Method Named singleRecipeData that allows the user to add the data of the Recipe

--- FullRecipe.cs
+++ FullRecipe.cs
@@ -11,8 +11,30 @@ namespace SanelesRecipeApplication
         // Creating an instance for the RecipeInformation Class
         RecipeInformation recipeInfo = new RecipeInformation();
 
-        
-
+        // A method that allows the user to enter in the details of a single recipe
+        public void singleRecipeData()
+        {
+            // Displaying a welcoming message for the application
+            Console.WriteLine("Hey there, Welcome To Sanele's Recipe App!");
+            // Asking the user to enter in the details for the recipe into the
+            Console.WriteLine("Please enter in the details for a single recipe: ");
+            recipeInfo.RecipeDetails = Console.ReadLine();
+            // Asking the user to specify the number of ingredients they want to enter and save it
+            Console.WriteLine("How many ingredients do you want to enter?");
+            string input = Console.ReadLine();
+            // Converting the input number for the number of ingredients into an int
+            int numberOfIngredients;
+            // Attempt to parse the input string to an integer
+            if (int.TryParse(input, out numberOfIngredients))
+            {
+                recipeInfo.NumberOfIngredients = numberOfIngredients;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
         }
+
+
     }
 }
